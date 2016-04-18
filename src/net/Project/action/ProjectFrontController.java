68875a2fc28e46cccf2c665a2cf5ce4a90e64fd3
llxoxll13 <llxoxll13@naver.com>
2016-04-19 03:02:47 +0900

    .

---
+++ src/net/Project/action/ProjectFrontController.java
@@ -0,0 +1,54 @@
+package net.Project.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class ProjectFrontController extends HttpServlet {
+
+	private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = requestURI.substring(contextPath.length());
+
+		ActionForward forward = null;
+		Action action = null;
+		System.out.println("command");
+		
+		if(command.equals("/main.do")){
+			forward = new ActionForward();
+			forward.setPath("./main/main.jsp");
+			forward.setRedirect(false);
+//			
+			
+		}
+		
+		if (forward != null) {
+			if (forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			} else {
+				RequestDispatcher dispacher = request.getRequestDispatcher(forward.getPath());
+				dispacher.forward(request, response);
+			}
+		}
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProcess(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProcess(request, response);
+	}
+	
+}
